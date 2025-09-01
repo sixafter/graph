2d0a70b8f310b10f5d2ddf58ebdb39284cf658e5
Michael Primeaux <mprimeaux@users.noreply.github.com>
2025-09-01 11:33:55 -0500

    debt: Upgrade dependencies (#7)

--- metrics/clustering_coefficient_test.go
+++ metrics/clustering_coefficient_test.go
@@ -1,6 +1,6 @@
 // File: metrics/clustering_coefficient_test.go
 
-// Copyright (c) 2024 Six After, Inc
+// Copyright (c) 2024-2025 Six After, Inc
 //
 // This source code is licensed under the Apache 2.0 License found in the
 // LICENSE file in the root directory of this source tree.
